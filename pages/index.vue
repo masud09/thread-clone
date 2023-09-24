4228851562662753a815afd23d5c5b6dcfefcc25
masud09 <mekanak909.mr@gmail.com>
2023-09-24 18:02:51 +0600

    test commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
     <MainLayout>
+
+        <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit nemo eius odit odio, provident dolorem quasi! Odit rem, eligendi tempore culpa sapiente numquam excepturi debitis ex sint doloremque asperiores delectus?</p>
         <div id="indexPage" class="w-full overflow-auto">
             <div class="mx-auto max-w-[500px] overflow-hidden">
                 <div id="Posts" class="px-4 max-w-[600px] max-auto">
