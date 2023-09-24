a3e78a56d55df6ddf6a86d171a2efcb2aa0e967d
masud09 <mekanak909.mr@gmail.com>
2023-09-25 00:48:19 +0600

    add post file

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
     <MainLayout>
 
-        <!-- <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit nemo eius odit odio, provident dolorem quasi! Odit rem, eligendi tempore culpa sapiente numquam excepturi debitis ex sint doloremque asperiores delectus?</p> -->
+       
         <div id="indexPage" class="w-full overflow-auto">
             <div class="mx-auto max-w-[500px] overflow-hidden">
                 <div id="Posts" class="px-4 max-w-[600px] max-auto">
-                    <div class="text-white" v-if="isPosts" v-for="post in posts" :key="post">
-                        {{ post }}
+                    <div v-if="isPosts" v-for="post in posts" :key="post">
+                        <Post :post="post" @isDeleted="posts=[]"/>
 
                     </div>
 
