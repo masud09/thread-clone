e46e6de74e17e978f34215cce12083fab5c457e0
masud09 <mekanak909.mr@gmail.com>
2023-09-25 13:44:35 +0600

    title dot click amd heart

--- components/Post.vue
+++ components/Post.vue
@@ -26,8 +26,66 @@
               size="18"
             />
           </button>
+          <div v-if="isMenu" class="absolute border border-gray-600 right-0 z-20 mt-1 rounded">
+
+            <button 
+            class="flex items-center rounded gap-2 text-red-500 justify-between bg-black w-full pl-4 pr-3 py-1 hover:bg-gray-900">
+
+            <div>
+              Delete
+            </div>
+            <Icon name="solar:trash-bin-trash-broken" size="20"/>
+
+        
+
+            </button>
+
+          </div>
         </div>
       </div>
+      <div class="relative flex items-center w-full">
+        <div class="w-[42px] max-auto">
+          <div class="absolute ml-4 mt-1 top-0 w-[1px] bg-gray-700 h-full"/>
+
+        </div>
+        <div class="bg-black rounded-lg w-[calc(100%-50px)] text-sm w-full font-light">
+          <div class="py-2 text-gray-300">
+            {{ post.text }}
+
+          </div>
+          <img 
+          v-if="post && post.picture"
+          class="mx-auto w-full mt-2 pr-2 rounded"
+          :src="post.picture"/>
+
+          <div class="absolute mt-2 w-full ml-2">
+            <button 
+            :disabled="isLike"
+            class="flex items-center gap-1">
+
+            <Icon 
+            class="p-1 text-white hover:bg-gray-800 rounded-full cursor-pointer" name="mdi:cards-heart-outline" size="28"/>
+
+            </button>
+            <div class="relative text-sm text-gray-500">
+              <div>
+                <span>4</span>
+                likes
+              </div>
+
+            </div>
+
+
+          </div>
+
+        </div>
+
+      </div>
+
+
+    </div>
+    <div class="relative inline-block text-gray-500 pt-1 pb-1.5">
+
     </div>
   </div>
 </template>
