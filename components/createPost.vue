2b5e8df6bb9dafcb3e6d317f20b5d01c48b2cfc8
masud09 <mekanak909.mr@gmail.com>
2023-09-27 02:37:58 +0600

    added text field in overlay

--- components/createPost.vue
+++ components/createPost.vue
@@ -1,90 +1,96 @@
 <template>
-    <div id="CreatePost" class="fixed z-50 bottom-0 h-full w-full overflow-hidden">
-        <div class="bg-black h-full w-full text-white overflow-auto">
-            <div class="flex items-center justify-start py-4 max-w-[500px] mx-auto border-b border-b-gray-700">
+  <div
+    id="CreatePost"
+    class="fixed z-50 bottom-0 h-full w-full overflow-hidden"
+  >
+    <div class="bg-black h-full w-full text-white overflow-auto">
+      <div
+        class="flex items-center justify-start py-4 max-w-[500px] mx-auto border-b border-b-gray-700"
+      >
+        <button
+          @click="
+            userStore.isMenuOverlay = false;
+            clearData();
+          "
+          class="rounded-full px-2"
+        >
+          <Icon name="mdi:close" size="25" />
+        </button>
 
-                <button
-                @click="
-                userStore.isMenuOverlay = false;
-                clearData();
-                "
-                class="rounded-full px-2"
-                >
-                    <Icon name="x" size="25"/>
-                </button>
+        <div class="text-[16px] font-semibold">New Thread</div>
+      </div>
 
-                <div class="text-[16px] font-semibold"> New Thread </div>
-
-                
-                   
-                
-
-                
-
-
+      <div
+        id="Post"
+        class="z-40 bottom-0 max-h-[100vh-200px] w-full px-3 max-w-[500px] mx-auto"
+      >
+        <div class="py-2 w-full">
+          <div class="flex items-center">
+            <div class="flex items-center text-white">
+              <img
+                class="rounded-full h-[35px]"
+                src="https://picsum.photos/id/223/50"
+                alt=""
+              />
+              <div class="ml-2 font-semibold text-[18px]">jon Weeks Dev</div>
             </div>
-            <div id="Post" class="z-40 bottom-0 max-h-[100vh-200px] w-full px-3 max-w-[500px]">
-
-<div class="py-2 w-full">
-    <div class=" flex items-center">
-
-        <div class="flex items-center text-white">
-            <img class="rounded-full h-[35px]" src="https://picsum.photos/id/223/50" alt=""/>
-            <div class="ml-2 font-semibold text-[18px]">jon Weeks Dev
-
+          </div>
+          <div class="relative flex items-center w-full">
+            <div class="w-[42px] max-auto">
+              <div
+                class="absolute ml-4 mt-1 top-0 w-[1px] bg-gray-700 h-full"
+              ></div>
             </div>
-
+            <div
+              class="bg-black rounded-lg w-[calc(100%-50px)] text w-full font-light"
+            >
+              <div class="pt-2 text-gray-300 bg-black w-full">
+                <textarea
+                  v-model="text"
+                  style="resize: none"
+                  placeholder="Start a thread..."
+                  id="textarea"
+                  @input="adjustTextareaHeight()"
+                  class="w-full bg-black outline-none"
+                ></textarea>
+              </div>
+            </div>
+          </div>
         </div>
-
+      </div>
     </div>
+  </div>
+ 
+</template>
 
-</div>
+<script setup>
+import { v4 as uuidv4 } from "uuid";
+import { useUserStore } from "~~/store/user";
+const userStore = useUserStore();
 
-</div>
-            
+// const client = useSupabaseClient()
+// const user = useSupabaseUser()
 
-            
-            
+let text = ref(null);
+let isLoading = ref(false);
+const adjustTextareaHeight = () => {
+  var textarea = document.getElementById("textarea");
+  textarea.style.height = "auto";
+  textarea.style.height = textarea.scrollHeight + "px";
+};
+let file = ref(null);
+let fileDisplay = ref(null);
+let fileData = ref(null);
 
-        </div>
-     
-     
-      
-    </div>
-  </template>
-  
-  <script setup>
-  import {v4 as uuidv4} from 'uuid';
-  import { useUserStore } from "~~/store/user";
-  const userStore = useUserStore();
-  
-  
-  // const client = useSupabaseClient()
-  // const user = useSupabaseUser()
+const clearData = () => {
+  text.value = null;
+  file.value = null;
+  fileDisplay.value = null;
+  fileData.value = null;
+};
 
-  let text = ref(null)
-  let isLoading = ref(false)
-  const adjustTextareaHeight = () =>{
-    var textarea = document.getElementById("textarea");
-    textarea.style.height= "auto"
-    textarea.style.height= textarea.scrollHeight + "px"
-  }
-    let file = ref(null)
-    let fileDisplay = ref(null)
-    let fileData = ref(null) 
-
-  const clearData = () =>{
-
-    text.value =null
-     file.value = null
-     fileDisplay.value= null
-     fileData.value = null
-  }
-
-  const onChange = () =>{
-
-    fileDisplay.value = URL.createObjectURL(file.value.files[0])
-    fileData.value = file.value.files[0]
-  }
-  </script>
-  
+const onChange = () => {
+  fileDisplay.value = URL.createObjectURL(file.value.files[0]);
+  fileData.value = file.value.files[0];
+};
+</script>
