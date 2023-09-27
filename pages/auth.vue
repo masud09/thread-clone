1f6aea47e7b652523e528b9de9aac1c77a64b995
masud09 <mekanak909.mr@gmail.com>
2023-09-27 11:30:09 +0600

    added auth successfully

---
+++ pages/auth.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="AuthPage" class="w-full h-[100vh] h-screen pt-32">
+
+        <div class="w-full">
+
+            <div class="w-full flex items-center justify-center gap-2.5 p-2">
+                <img class="w-[35px]" src="/threads-logo.png" alt="">
+                <span class="font-bold text-2xl text-white">Threads</span>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</template>
+<script setup>
+// const client = useSupabaseClient()
+// const user = useSupabaseUser()
+// watchEffect(()=>{
+
+//     if (user.value){
+//         return navigateTo('/')
+//     }
+// })
+
+// const login = async (prov) =>{
+
+//     const { data, error } = await client.auth.signInWithOAuth({
+//         provider: prov,
+//         redirectTo: window.location.origin
+//     })
+//     if(error) console.log(error)
+// }
+</script>
